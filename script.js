d058b2d3c889a749f5d21804d64ada63f28bfb62
agataxmascarenhas <93822452+agataxmascarenhas@users.noreply.github.com>
2023-03-24 03:14:08 +0000

    updated header

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+window.addEventListener("load", function () {
+	setTimeout(function () {
+	  // Vai adicionar mais um class chamado mystyle em 0.5s
+	  // Significa que vai aplicar um transform: translate(-50%, -50%); em 0.5s no div do popup
+		document.querySelector(".header-content").classList.add("mystyle");
+	}, 500);
+});
